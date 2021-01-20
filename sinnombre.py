d4aca5e42ae72f966362a583ed5268b4d7d010f2
Irenegdp94 <77295901+Irenegdp94@users.noreply.github.com>
2021-01-20 20:20:08 +0100

    carrera bichos

--- sinnombre.py
+++ sinnombre.py
@@ -1,9 +1,23 @@
 import pygame,sys
+import random
 width = 640
 height = 480
+
+class Runner():
+    __customes = ("turtle", "fish" , "prawn", "moray", "octopus")
+    def __init__(self,x=0,y=0):
+        ixCustome = random.randint(0,4)
+        self.custome = pygame.image.load("imagen/{}.png".format(self.__customes[ixCustome]))
+        self.position = [x,y]
+        self.name = ""
+    
+    def avanzar(self):
+        self.position[0] += random.randint(1,6)
+
 class Game():
     runners = []
-    __starLine = 20
+    __posY = [160,200,240,280]
+    __starLine = -20
     __finishLine = 620
     
     def __init__(self):
@@ -12,17 +26,38 @@ class Game():
         self.__background = pygame.image.load("imagen/background.png")
         pygame.display.set_caption("Carrera")
         
+        for i in range(4):
+            theRunner = Runner(self.__starLine,self.__posY[i])
+            self.runners.append(theRunner)
+    def close(self):
+        pygame.quit()
+        sys.exit()
     def competir(self):
         gameOver = False
         while not gameOver:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     gameOver = True
+            
+            for activeRunner in self.runners:
+                activeRunner.avanzar()
+                
+                if activeRunner.position[0] >= self.__finishLine:
+                    print("{} ha ganado!".format(activeRunner.name))
+                    gameOver = True
+            
             self.__screen.blit(self.__background,(0,0))
+            
+            for runner in self.runners:
+                self.__screen.blit(runner.custome,runner.position)
+            
             pygame.display.flip()
         
-        pygame.quit()
-        sys.exit()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.close()
+        
 
 if __name__ == "__main__":
     game = Game()
